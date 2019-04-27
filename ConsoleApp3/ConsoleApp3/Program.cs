01f5dbcc44f963cb0707fd77bfc164ec86068e62
Q79 <mantas.sviklas@gmail.com>
2019-04-27 13:16:34 +0300

    Uzduotys

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Namu_Uzduotis1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string spalva = "";
+            while (spalva != " ")
+            {
+                Console.WriteLine("Iveskite savo megstamiausia spalva:");
+                spalva = Console.ReadLine();
+                Console.WriteLine("Mano megstamiausia spalva irgi {0} ", spalva);
+            }
+           
+        }
+    }
+}
